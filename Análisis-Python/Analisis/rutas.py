29aeb3f9c1b5fd29f564b326a36a861c2b25a5e1
Danfer Marcelo Ore <danfer.ore.martinez@gmail.com>
2026-08-26 18:56:34 -0500

    Add files via upload

---
+++ Análisis-Python/Analisis/rutas.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+ruta = Path(__file__).parents[2] / 'Archivos'
+
+def ruta_archivo_original()-> Path:
+    return ruta / 'data_jobs.csv' 
+
+def ruta_archivo_limpio()-> Path:
+    return ruta / 'data_jobs_limpio.parquet' 
